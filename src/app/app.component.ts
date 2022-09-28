0889b073364be2d7bd0cf74d16992dcb9896d194
Juan Revelo <jreveloj@xoftix.com>
2022-09-28 14:29:53 -0500

    Editor base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
+import * as htmlDocx from 'html-docx-js-typescript';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'editor';
+
+  htmlDocument = '<!DOCTYPE html> <html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge">    <meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head>';
+  htmlContent = '';
+
+  config: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '15rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',
+    defaultParagraphSeparator: 'p',
+    defaultFontName: 'Arial',
+  };
+
+  saveDocx(){
+    this.htmlDocument + '<body>' + this.htmlContent + '</body></html>';
+    htmlDocx.asBlob(this.htmlDocument).then( (data: any) => {
+      saveAs(data, 'plantilla1.docx') // save a docx file
+    });
+  }
+
+  
 }
